704c62e1e7d9a56f2a560cbefa37b6edd7a63c36
lynx <1197225628@qq.com>
2019-12-03 19:02:45 +0800

    first

---
+++ app/src/main/java/com/example/music_redo/MainPlayer.java
@@ -0,0 +1,14 @@
+package com.example.music_redo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
