2a9c17181090ea0340411efe10109071c4a2169a
lynx <2104934038@qq.com>
2019-12-03 20:00:37 +0800

    清空media信号接收,todo:创建歌单

--- app/src/main/java/com/example/music_redo/MediaReceiver.java
+++ app/src/main/java/com/example/music_redo/MediaReceiver.java
@@ -16,7 +16,7 @@ public class MediaReceiver extends BroadcastReceiver {
         ;// 不能直接调用 mContext
     }
 
-    public MediaReceiver(Context context) {// TODO 暂不清楚监测出现重复是否是因为有参构造方法造成
+    public MediaReceiver(Context context) {
         this.myContext = context;
     }
 
@@ -31,19 +31,15 @@ public class MediaReceiver extends BroadcastReceiver {
                 case Intent.ACTION_HEADSET_PLUG:
                     int mediaState = intent.getIntExtra("state", 0);// 判断插拔
                     if (mediaState == 0) {// 拔出耳机
-                        if (MainPlayer.player.isPlaying()) {
-                            MainPlayer.button_play.callOnClick();// TODO 强制暂停
-                        }
-                        MainPlayer.infoLog("remove headset");
+                        // TODO 强制暂停
                     } else if (mediaState == 1) {// 插入耳机
-                        MainPlayer.infoLog("plug headset");
                     }
                     break;
 
                 // 蓝牙连接状态改变
-                case BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED:// 安卓端主动改变蓝牙状态
+                // 安卓端主动改变蓝牙状态
+                case BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED:
                     int bluetoothState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, 0);// 获取蓝牙状态
-                    MainPlayer.infoToast(myContext, "bluetooth state todo");// TODO
                     switch (bluetoothState) {
                         case BluetoothAdapter.STATE_TURNING_ON:
                             MainPlayer.infoLog("turning on");
@@ -59,14 +55,10 @@ public class MediaReceiver extends BroadcastReceiver {
                             break;
                     }
                     break;
-                case BluetoothDevice.ACTION_ACL_CONNECTED:// 蓝牙设备主动改变状态
-                    MainPlayer.infoLog("connected");
+                // 蓝牙设备主动改变状态
+                case BluetoothDevice.ACTION_ACL_CONNECTED:// 蓝牙连接设备
                     break;
-                case BluetoothDevice.ACTION_ACL_DISCONNECTED:// 蓝牙设备主动改变状态
-                    MainPlayer.infoLog("disconnected");
-                    if (MainPlayer.player.isPlaying()) {
-                        MainPlayer.button_play.callOnClick();// TODO 强制暂停
-                    }
+                case BluetoothDevice.ACTION_ACL_DISCONNECTED:// 蓝牙断开设备
                     break;
 
                 // 接收蓝牙/媒体按键信号
@@ -87,44 +79,10 @@ public class MediaReceiver extends BroadcastReceiver {
                             break;
                         case KeyEvent.KEYCODE_HEADSETHOOK:// 播放/暂停 79
                             // TODO 切歌
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Long tmp = System.currentTimeMillis();
-                                    Long timeDiff = tmp - MainPlayer.myTime;
-                                    MainPlayer.myTime = tmp;
-                                    MainPlayer.infoLog("time diff: " + timeDiff);
-                                    if (timeDiff < 500) {// TODO 累计
-                                        MainPlayer.clickTimes ++;
-                                    }
-
-                                    int last_click_times = MainPlayer.clickTimes;// 之前累积的次数
-                                    try {
-                                        Thread.sleep(500);// TODO 延迟
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-
-                                    if (last_click_times == MainPlayer.clickTimes) {// TODO 忽略4次以上的点击
-                                        if (last_click_times == -1) {
-                                            MainPlayer.infoLog("click -1 time???");
-                                        } else if (last_click_times == 0) {
-                                        } else if (last_click_times == 1) {// TODO 下一首
-                                            MainPlayer.infoLog("todo next");
-                                        } else if (last_click_times == 2) {// TODO 上一首
-                                            MainPlayer.infoLog("todo last");
-                                        }
-                                        MainPlayer.clickTimes = 0;// TODO 累计清零
-                                    } else {
-                                        MainPlayer.infoLog("click times: " + last_click_times + "/" + MainPlayer.clickTimes);
-                                    }
-                                }
-                            }).start();
                             break;
                         case KeyEvent.KEYCODE_MEDIA_PLAY:// 播放 126
                         case KeyEvent.KEYCODE_MEDIA_PAUSE:// 暂停 127
-                            // TODO 避免重复检测
-                            MainPlayer.button_play.callOnClick();// TODO 无差别对待 播放和暂停
+                            // TODO 无差别对待 播放和暂停
                             break;
                     }
                     break;
