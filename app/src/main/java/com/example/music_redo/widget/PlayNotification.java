d43ee0a5257e0ce47705586b51e87dd2e23e06fc
lynx <2104934038@qq.com>
2019-12-11 20:02:40 +0800

    bug:不显示通知栏

--- app/src/main/java/com/example/music_redo/widget/PlayNotification.java
+++ app/src/main/java/com/example/music_redo/widget/PlayNotification.java
@@ -1,16 +1,19 @@
 package com.example.music_redo.widget;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.widget.RemoteViews;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import com.example.music_redo.MusicList;
 import com.example.music_redo.R;
 
 public class PlayNotification extends Service {
@@ -19,6 +22,9 @@ public class PlayNotification extends Service {
 
     String packageName;
 
+    NotificationCompat.Builder builder;
+    NotificationChannel channel;
+    NotificationManager manager;
     RemoteViews remoteViews;
 
     @Override
@@ -35,7 +41,7 @@ public class PlayNotification extends Service {
     }
 
     public void initData() {
-        if (remoteViews == null) {// TODO
+        if (remoteViews != null) {// TODO
             remoteViews.removeAllViews(R.layout.play_notification);
             remoteViews = null;
         }
@@ -43,21 +49,22 @@ public class PlayNotification extends Service {
     }
 
     public void initView() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    NotificationCompat.Builder builder = new NotificationCompat.Builder(myContext);
-                    RemoteViews remoteViews = new RemoteViews(packageName, R.layout.play_notification);
-                    NotificationManager manager = (NotificationManager) myActivity.getSystemService(Context.NOTIFICATION_SERVICE);
-
-                    builder.setSmallIcon(R.drawable.ic_launcher_background);
-                    builder.setCustomContentView(remoteViews);
-                    builder.setStyle(new NotificationCompat.DecoratedCustomViewStyle());
-
-                    manager.notify(1, builder.build());// TODO id
-                }
-            }).start();
+        if (channel == null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {// TODO
+                channel = new NotificationChannel("default", "default", NotificationManager.IMPORTANCE_DEFAULT);
+                manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.createNotificationChannel(channel);
+            }
         }
+
+        if (builder == null) {
+            builder = new NotificationCompat.Builder(this, "default")
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContent(remoteViews)
+                    .setOnlyAlertOnce(false)// TODO
+                    .setAutoCancel(false)// TODO
+                    .setOngoing(true);// TODO
+        }
+
     }
 }
